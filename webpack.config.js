416606af15e00e92f252f732449889c4054d28ce
= <=>
2022-01-03 18:17:36 +0100

    sass & browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+let target = process.env.NODE_ENV === 'production' ? 'browserslist' : 'web';
 
 module.exports = {
     mode: mode,
+    target: target,
 
     module: {
         rules: [
+            {
+                test: /\.s?css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -15,8 +28,13 @@ module.exports = {
         ]
     },
 
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
+
     devtool: 'source-map',
     devServer: {
-        contentBase: './dist'
+        static: './dist',
+        hot: true
     }
 }
